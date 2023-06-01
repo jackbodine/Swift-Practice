56941006ab5fcb4f50378fdf234407b7ec9d8734
Jack Bodine <JackTaylorBodine@gmail.com>
2023-05-31 19:23:18 -0500

    Finished lecture 8

--- Stanford 193p/SwiftUI-Practice/EmojiMemoryGameView.swift
+++ Stanford 193p/SwiftUI-Practice/EmojiMemoryGameView.swift
@@ -131,11 +131,26 @@ struct EmojiMemoryGameView: View {
 struct CardView: View {
     let card: EmojiMemoryGame.Card   // Only pass into it the minimum it needs. not the whole viewModel.
     
+    @State private var animatedBonusRemaining: Double = 0
+    
     var body: some View {
         GeometryReader { geometry in
             ZStack {
-                Pie(startAngle: Angle(degrees: 270), endAngle: Angle(degrees: 20)).padding(5)
-                        .opacity(0.5)
+                Group {
+                    if card.isConsumingBonusTime {
+                        Pie(startAngle: Angle(degrees: 270), endAngle: Angle(degrees: (1-animatedBonusRemaining)*360-90))
+                            .onAppear() {
+                                animatedBonusRemaining = card.bonusRemaining
+                                withAnimation(.linear(duration: card.bonusTimeRemaining)) {
+                                    animatedBonusRemaining = 0
+                                }
+                            }
+                    } else {
+                        Pie(startAngle: Angle(degrees: 270), endAngle: Angle(degrees: (1-card.bonusTimeRemaining)*360-90))
+                    }
+                }
+                    .padding(5)
+                    .opacity(0.5)
                 Text(card.content)
                     .rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
                     .animation(Animation.easeInOut(duration: 1))
